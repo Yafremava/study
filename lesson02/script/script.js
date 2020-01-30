7121b397a167f376e626d58dd9ab840679f529d7
Efremova Ekaterina <kayfremava@yandex.by>
2020-01-30 22:15:48 +0100

    Исправление второго урока

--- lesson02/script/script.js
+++ lesson02/script/script.js
@@ -10,3 +10,10 @@ console.log(typeof income);
 console.log(typeof deposit);
 
 console.log(addExpenses.length());
+
+console.log('Период равен '+ period +' месяцев');
+console.log('Цель заработать '+ mission +' рублей/долларов/гривен/юани');
+console.log(addExpenses.toLowerCase().split());
+
+let budgetDay = money / 30;
+console.log(budgetDay);
