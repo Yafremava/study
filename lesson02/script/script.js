3213f4ed5b52ca98a83e472282778f2fd3896473
Efremova Ekaterina <kayfremava@yandex.by>
2020-01-30 21:35:43 +0100

    first commit

---
+++ lesson02/script/script.js
@@ -0,0 +1,12 @@
+let money = 600,
+  income = 'фриланс',
+  addExpenses = 'кафе, косметос, шмотки',
+  deposit = true,
+  mission = 1000,
+  period = 4;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length());
