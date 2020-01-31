c5fe200d79de4c190c46dda9255c4dd9ab027c86
Efremova Ekaterina <kayfremava@yandex.by>
2020-01-31 09:27:54 +0100

    lesson03

--- lesson03/script/script.js
+++ lesson03/script/script.js
@@ -17,7 +17,7 @@ console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
 
-console.log('Длина строки addExpenses: '+ addExpenses.length());
+console.log('Длина строки addExpenses: '+ addExpenses.length);
 
 console.log('Период равен ' + period + ' месяцев');
 
