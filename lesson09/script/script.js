4ec17f1e42fa6008740b15cde0ac6f7c69b8c55c
Efremova Ekaterina <kayfremava@yandex.by>
2020-02-10 18:07:53 +0300

    lesson09

--- lesson09/script/script.js
+++ lesson09/script/script.js
@@ -1,27 +1,28 @@
 'use strict';
 
-let button = document.getElementById('start'),
+let button = document.getElementById('start'),//Кнопку "Рассчитать"
+
   plusButton1 = document.getElementsByTagName('h1')[0],
   plusButton2 = document.getElementsByTagName('h1')[0],
+
   checkbox = document.querySelector('#deposit-check'),
-  possibleIncome1 = document.querySelectorAll('.additional_income-item')[0],
+
+  possibleIncome1 = document.querySelectorAll('.additional_income-item')[0],//поля возможных доходов
   possibleIncome2 = document.querySelectorAll('.additional_income-item')[1],
-  budgetDay = document.getElementsByClassName('result-total budget_day-value'),
+
+  budgetDay = document.getElementsByClassName('result-total budget_day-value'),//элементы по правой стороне
   expensesMonth = document.getElementsByClassName('result-total expenses_month-value'),
   additionalIncome = document.getElementsByClassName('result-total additional_income-value'),
   additionalExpenses = document.getElementsByClassName('result-total additional_expenses-value'),
   incomePeriod = document.getElementsByClassName('result-total income_period-value'),
   targetMonth = document.getElementsByClassName('result-total target_month-value'),
-  salaryAmount = document.querySelector('.salary-amount'),
+
+  salaryAmount = document.querySelector('.salary-amount'),// поля по левой стороне, кроме возможных доходов
   incomeTitle = document.querySelector('.income-title'),
   incomeAmount = document.querySelector('.income-amount'),
-  expensesTitle = document.querySelector('.expenses-title'),
-  additionalExpensesItem = document.querySelector('.additional_expenses-item');
+  esTexpensitle = document.querySelector('.expenses-title'),
+  additionalExpensesItem = document.querySelector('.additional_expenses-item'),
+  targetAmount = document.querySelector('.target-amount'),
+  periodSelect = document.querySelector('.period-select');
 
-
-console.log(button);
-console.log(plusButton1);
-console.log(plusButton2);
-console.log(checkbox);
-console.log(possibleIncome1);
-console.log(possibleIncome2);
+console.log(budgetDay);
